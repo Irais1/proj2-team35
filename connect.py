4059f3b4e9d0a848e96862c1b4ffeda2257c0d0e
Irais Gopar <igopar-osorio@csumb.edu>
2017-03-17 23:37:17 -0700

    Update connect.py

--- connect.py
+++ connect.py
@@ -54,7 +54,78 @@ def index():
 def results():
     return render_template("results.html")
     
+def getAudio():from flask import Flask, render_template, request, url_for, redirect, url_for
+#from string import Template
+#import Requests
+import os
+
+from beats import mixAudio
+#from Werkzeug.utils import secure_filename
+
+
+#HTML_TEMPLATE = Template
+music_dir = '/home/flask/flaskmedia/static/music'
+
+UPLOAD_FOLDER = '/home/flask/flaskmedia/static/music'
+ALLOWED_EXTENSIONS = set(['wav'])
+#music_dir2 = '/home/flask/flaskmedia/static/music'
+#music_dir3 = '/home/flask/flaskmedia/static/music'
+app = Flask(__name__, template_folder='template')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+@app.route('/')
+def homepage():
+	return render_template('proj2.html')
+@app.route('/allowed_file')	
+def allowed_file(filename):
+	return '.' in filename and \
+	filename.rsplit('.',1)[1].lower()in ALLOWED_EXTENSIONS
+@app.route('/storedWav')
+def storeWav(myWav):
+	store.append(myWav)
+	
+@app.route('/upload_file',methods = ['GET', 'POST'])	
+def upload_file():
+	if request.method == 'POST':
+		flash('No file part')
+		return redirect(request.url)
+	file = request.files['file']
+	if file.filename=='':
+		flash('No file selected')
+		return redirect (request.url)
+	if file and allowed_file(file.filename):
+		file_name= secure_file(file.filename)
+		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+		storeWav(filename)
+		return redirect (url_for('uploaded_file'))
+	return render_template('proj2.html')
+			
+	
+@app.route('/')
+@app.route('/home')
+def index():
+	music_files = [f for f in os.listdir(music_dir) if f.endswith('wav')]
+	music_files_number = len(music_files)
+	return render_template("proj2.html",
+               title = 'Home',
+               music_files_number = music_files_number,
+               music_files = music_files)
+
+	
+@app.route('/results', methods = ['GET', 'POST'])
+def results():
+    return render_template("results.html")
+@app.route('/getAudio')    
 def getAudio():
+    mixFile = mixAudio("Bailando.wav", "tearinmyheart.wav", "riverflowsinyou.wav")
+    return mixFile
+app.jinja_env.globals.update(getAudio=getAudio)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True, port = int (os.getenv('PORT', 8080)), host = os.getenv('IP', '0.0.0.0'))
+ 
+    #app.run('/home')
+
+
     mixFile = mixAudio()
     return mixFile
 app.jinja_env.globals.update(getAudio=getAudio)
